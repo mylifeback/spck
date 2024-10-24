98b290058bb4a22e5c701bbb26cc70cb1f45b43a
m's experiments <mauricewong@me.com>
2024-10-24 17:44:31 +0800

    d

--- select/data.js
+++ select/data.js
@@ -1,8 +1,8 @@
 
 const a1 = ["lesion", "red", 1.0];
 const a2 = ["skin", "green", 0.7];
-const a3 = ["dura", "blue", 0.8, "1"];
-const a4 = ["opening", "purple", 0.8, "2"];
+const a3 = ["dura", "blue", 1, "1"];
+const a4 = ["opening", "purple", 1, "2"];
 const a5 = ["rightSkin", "green", 0.8, "3"];
 const a6 = ["marker", 0xFFFFFF, 0.8, "4"];
 const models = [a1, a2, a3, a4, a5, a6];
