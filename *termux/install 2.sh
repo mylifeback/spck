63c347aee2a1ee1f05ebe4f37cdf46f8fa1e7f0a
mylifeback <mauricewong@mac.com>
2025-02-09 15:30:27 +0800

    update

--- *termux/install 2.sh
+++ *termux/install 2.sh
@@ -1,2 +1,11 @@
 #!/data/data/com.termus/files/usr/bin/bash
 pkg install dropbear -y
+
+termux-setup-storage
+
+passwd
+
+vncconfig &
+(or vncconfig -nowin)
+vncpasswd
+
--- *termux/install.sh
+++ *termux/install.sh
@@ -1,3 +1,4 @@
 #!/data/data/com.termus/files/usr/bin/bash
-pkg update -y && pkg upgrade -y && pkg install htop openssh x11-repo tigervnc xfce4-session xfce4-terminal proot-distro fluxbox tmux termux-api termux-auth root-repo tur-repo -y
+pkg update -y && pkg upgrade -y && pkg install htop openssh tmux proot-distro termux-tools termux-api termux-auth -y
+pkg install x11-repo tigervnc xfce4-session xfce4-terminal fluxbox root-repo tur-repo -y
 
