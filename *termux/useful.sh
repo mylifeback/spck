95c72f5ed2a3447b8dfbd696ae76af7208b637b9
mylifeback <mauricewong@mac.com>
2025-02-12 09:40:19 +0800

    update

---
+++ *termux/useful.sh
@@ -0,0 +1,14 @@
+
+
+BASHDOT
+curl -s https://raw.githubusercontent.com/bashdot/bashdot/master/bashdot > bashdot
+bashdot install XXX
+mv .xxx XXX/xxx
+
+TREE
+
+DU
+show file/directory by size (sorted)
+du -a -h --max-depth=1 | sort -hr
+
+
