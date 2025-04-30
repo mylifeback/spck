61b529ca87cccb6ece22cb5fe64b1645c9383a30
mylifeback <mauricewong@mac.com>
2025-04-30 19:11:20 +0800

    update

--- codepen/test.js
+++ codepen/test.js
@@ -1,1 +1,12 @@
+
 console.dir(Math.PI);
+setInterval (hello, 1000);
+
+let b = 1;
+const a = document.getElementsByClassName("rotate");
+function hello (){
+  console.log(b);
+  a[0].textContent = parseInt(b);
+//  a[0].textContent = "ahhlsdf";
+  b ++;
+}
