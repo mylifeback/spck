cb6a770ea64fe88a709e815b661054e538890c8f
m's experiments <mauricewong@me.com>
2025-05-03 14:23:50 +0800

    r

--- ml5.js
+++ ml5.js
@@ -2,7 +2,7 @@ let CLASSIFIER = "MobileNet";
 const image = document.getElementById("image");
 const result = document.getElementById("result");
 const probability = document.getElementById("probability");
-const output = document.getElementById("output");
+const outputElement = document.getElementById("output");
 
 const classifier = ml5.imageClassifier(CLASSIFIER);
 
@@ -16,13 +16,13 @@ function onImageReady(){
   console.log("loaded");
   classifier.classify(image, 10, gotResult);
   console.log("predicted");
-  output.textContent = "predicting ...";
+  outputElement.textContent = "predicting ...";
 }
 
 function gotResult(results){
   console.log("got result ...");
   console.log({ results });
-  output.textContent = "results arrived ...";
+  outputElement.textContent = "results arrived ...";
   result.innerText = results[0].label;
   probability.innerText = results[0].probability.toPrecision(2);
 }
