695864bb38ca9d0d3588ab414b56bc5a6c3cc244
m's experiments <mauricewong@me.com>
2025-05-03 13:55:12 +0800

    3

--- ml5.js
+++ ml5.js
@@ -2,6 +2,7 @@ let CLASSIFIER = "MobileNet";
 const image = document.getElementById("image");
 const result = document.getElementById("result");
 const probability = document.getElementById("probability");
+const log = document.getElementById("console");
 
 const classifier = ml5.imageClassifier(CLASSIFIER);
 
@@ -15,11 +16,13 @@ function onImageReady(){
   console.log("loaded");
   classifier.classify(image, 10, gotResult);
   console.log("predicted");
+  log.textContent = "predicting ...";
 }
 
 function gotResult(results){
   console.log("got result ...");
-  console.log({results});
+  console.log({ results });
+  log.textContent = "results arrived ...";
   result.innerText = results[0].label;
   probability.innerText = results[0].probability.toPrecision(2);
 }
