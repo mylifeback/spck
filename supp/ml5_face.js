c2ebccf5a87d74173c8628a959743f02c4931ac3
m's experiments <mauricewong@me.com>
2025-05-03 17:51:03 +0800

    f

--- supp/ml5_face.js
+++ supp/ml5_face.js
@@ -12,9 +12,6 @@ const constraints = {
     height: {min: 240, ideal: 480, max: 720}
   }
 }
-const source = "https://";
-
-
 
 preload();
 camera();
@@ -39,6 +36,8 @@ function got1Face(results, error) {
 function setup() {
   // faceMesh.detectStart(video, gotFaces);
   faceMesh.detect(video, got1Face);
+  let triangles = faceMesh.getTriangles();
+  console.log({ triangles });
 
 }
 
@@ -49,24 +48,3 @@ function camera() {
   })
 }
 
-// console.log(image);
-// console.log(result);
-// console.log(probability);
-// console.log(outputElement);
-// onImageReady();
-
-// function onImageReady(){
-//   console.log("loaded");
-//   classifier.classify(image, 10, gotResult);
-//   console.log("predicted");
-//   console.log(outputElement);
-//   outputElement.textContent = "predicting ...";
-// }
-
-// function gotResult(results){
-//   console.log("got result ...");
-//   console.log({ results });
-//   outputElement.textContent = "results arrived ...";
-//   result.innerText = results[0].label;
-//   probability.innerText = results[0].confidence.toPrecision(2);
-// }
